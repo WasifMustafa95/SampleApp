a9797fab088aa0cc230966983d3fbb019ca5518b
Wasif Mustafa <wasif_mahmood95@hotmail.com>
2023-02-11 08:43:29 +0500

    ...

---
+++ NetworkVerificationApp/NetworkVerificationApp/NetworkVerificationApp.iOS/GetSSIDiOS.cs
@@ -0,0 +1,34 @@
+﻿using Foundation;
+using NetworkVerificationApp.Interfaces;
+using NetworkVerificationApp.iOS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SystemConfiguration;
+using UIKit;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(GetSSIDiOS))]
+namespace NetworkVerificationApp.iOS
+{
+    public class GetSSIDiOS : IGetNetworkName
+    {
+        string ssid;
+        public string GetSSID()
+        {
+            if (CaptiveNetwork.TryGetSupportedInterfaces(out string[] supportedInterfaces) == StatusCode.OK)
+            {
+                foreach (var item in supportedInterfaces)
+                {
+                    if (CaptiveNetwork.TryCopyCurrentNetworkInfo(item, out NSDictionary info) == StatusCode.OK)
+                    {
+                        ssid = info[CaptiveNetwork.NetworkInfoKeySSID].ToString();
+                    }
+                }
+            }
+
+            return ssid;
+        }
+    }
+}
---
+++ NetworkVerificationApp/NetworkVerificationApp/NetworkVerificationApp/Interfaces/IGetNetworkName.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetworkVerificationApp.Interfaces
+{
+    public interface IGetNetworkName
+    {
+        string GetSSID();
+    }
+}
