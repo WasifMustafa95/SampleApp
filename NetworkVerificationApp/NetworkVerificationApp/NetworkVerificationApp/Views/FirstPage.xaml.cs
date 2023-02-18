f2d8700bcdc761bc4a6577192a53dafeb71e27fb
Wasif Mustafa <wasif_mahmood95@hotmail.com>
2023-02-19 04:01:59 +0500

    ....

--- NetworkVerificationApp/NetworkVerificationApp/NetworkVerificationApp/Views/FirstPage.xaml.cs
+++ NetworkVerificationApp/NetworkVerificationApp/NetworkVerificationApp/Views/FirstPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using NetworkVerificationApp.Interfaces;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +15,7 @@ namespace NetworkVerificationApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class FirstPage : ContentPage
     {
+        public List<Codes> Codes { get; set; }
         public FirstPage()
         {
             InitializeComponent();
@@ -22,6 +25,9 @@ namespace NetworkVerificationApp.Views
         {
             base.OnAppearing();
 
+            var code = GetCode();
+            codeLabel.Text = code.name;
+
             var ssid = DependencyService.Get<IGetNetworkName>().GetSSID();
             netwrokLabel.Text = ssid;
 
@@ -29,9 +35,33 @@ namespace NetworkVerificationApp.Views
             Preferences.Set("Code", codeLabel.Text);
         }
 
+        private Codes GetCode()
+        {
+            var assembly = typeof(FirstPage).Assembly;
+            foreach (var res in assembly.GetManifestResourceNames())
+            {
+                if (res.Contains("code.json"))
+                {
+                    Stream stream = assembly.GetManifestResourceStream(res);
+                    using (var reader = new StreamReader(stream))
+                    {
+                        var jsonData = reader.ReadToEnd();
+                        Codes = JsonConvert.DeserializeObject<List<Codes>>(jsonData);
+                        //CountryName = CodeList.Select(x => x.name).ToList();
+                    }
+                }
+            }
+            return Codes.FirstOrDefault();
+        }
+
         private void Button_Clicked(object sender, EventArgs e)
         {
             Navigation.PushAsync(new SecondPage());
         }
+    }
+
+    public class Codes
+    {
+        public string name { get; set; }
     }
 }
